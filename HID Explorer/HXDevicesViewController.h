bdbb144e8505a2e9b41d15c084efa682a213994f
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 05:54:10 -0600

    Files added.

---
+++ HID Explorer/HXDevicesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXDevicesViewController.h
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/10/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HXDevicesViewController : NSViewController
+
+@end
---
+++ HID Explorer/HXDevicesViewController.m
@@ -0,0 +1,31 @@
+//
+//  HXDevicesViewController.m
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/10/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HXDevicesViewController.h"
+
+@interface HXDevicesViewController ()
+
+@end
+
+@implementation HXDevicesViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	
+	// Do any additional setup after loading the view.
+	
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+	[super setRepresentedObject:representedObject];
+	
+	// Update the view, if already loaded.
+	
+}
+
+@end

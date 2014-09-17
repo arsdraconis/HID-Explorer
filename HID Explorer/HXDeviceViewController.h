32b01fa93b4f92233c74d35dbceec493a62e7137
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-17 04:47:31 -0600

    Added Device window and view controller.

---
+++ HID Explorer/HXDeviceViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXDeviceViewController.h
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/17/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HXDeviceViewController : NSViewController
+
+@end
---
+++ HID Explorer/HXDeviceViewController.m
@@ -0,0 +1,22 @@
+//
+//  HXDeviceViewController.m
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/17/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HXDeviceViewController.h"
+
+@interface HXDeviceViewController ()
+
+@end
+
+@implementation HXDeviceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

e2edacd9607e611036296d189d6e721b51887aa3
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-18 05:23:55 -0600

    Added HXDevicesWindowController.

---
+++ HID Explorer/HXDevicesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  HXDevicesWindowController.h
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/18/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HXDevicesWindowController : NSWindowController
+
+@end
---
+++ HID Explorer/HXDevicesWindowController.m
@@ -0,0 +1,23 @@
+//
+//  HXDevicesWindowController.m
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 9/18/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HXDevicesWindowController.h"
+
+@interface HXDevicesWindowController ()
+
+@end
+
+@implementation HXDevicesWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+	
+	[self.window setExcludedFromWindowsMenu:YES];
+}
+
+@end

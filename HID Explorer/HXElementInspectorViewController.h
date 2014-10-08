8659beb5ae8c492e1aafb867fa237ff487b87325
Robert Luis Hoover <shiroholloway@gmail.com>
2014-10-08 02:06:23 -0600

    Files added.

---
+++ HID Explorer/HXElementInspectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXElementInspectorViewController.h
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 10/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXElementInspectorViewController : NSObject
+
+@end
---
+++ HID Explorer/HXElementInspectorViewController.m
@@ -0,0 +1,13 @@
+//
+//  HXElementInspectorViewController.m
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 10/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HXElementInspectorViewController.h"
+
+@implementation HXElementInspectorViewController
+
+@end

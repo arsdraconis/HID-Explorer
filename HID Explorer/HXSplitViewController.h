79fa0312c75a852842ee893c24bef61077c457de
Robert Luis Hoover <shiroholloway@gmail.com>
2014-10-16 03:25:01 -0600

    Files added. Not sure if I'll need them.

---
+++ HID Explorer/HXSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXSplitViewController.h
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 10/15/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HXSplitViewController : NSSplitViewController
+
+@end
---
+++ HID Explorer/HXSplitViewController.m
@@ -0,0 +1,24 @@
+//
+//  HXSplitViewController.m
+//  HID Explorer
+//
+//  Created by Robert Luis Hoover on 10/15/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HXSplitViewController.h"
+
+@interface HXSplitViewController ()
+
+@property IBOutlet NSView *sidebarView;
+
+@end
+
+@implementation HXSplitViewController
+
+- (IBAction)toggleSidebar:(id)sender
+{
+	
+}
+
+@end
